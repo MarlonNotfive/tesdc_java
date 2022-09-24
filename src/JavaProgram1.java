9baed2ee128be752a9a15d574de9616df013b914
Marlon <marlond643@gmail.com>
2022-09-24 19:31:35 +0800

    First commit

---
+++ src/JavaProgram1.java
@@ -0,0 +1,18 @@
+public class JavaProgram1 {
+    public static void main(String args[]){
+        String name = "marlon";
+        System.out.println("hello");
+        //int
+        int number = 1;
+        System.out.println("the int is "+ number);
+
+        double floatedNumber = 2.123;
+        System.out.println("the double is "+ floatedNumber);
+
+        boolean isPerson = true;
+        System.out.println("the boolean is "+ isPerson);
+
+        char letter = 'A';
+        System.out.println("the letter is "+ letter);
+    }
+}
